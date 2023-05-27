eac2c24d8d2868990008eb8c53978d2c50bd5b16
shiva-charan-git <shivacharan021@gmail.com>
2023-05-27 16:42:26 +0530

    M

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 cp configs/mongodb.repo  /etc/yum.repos.d/mongo.repo
 yum install mongodb-org -y 
 systemctl enable mongod 
-systemctl start mongod b
+
 
 #Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
 
