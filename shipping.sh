155a99840eecf1a7897cced5871053d012e0705a
shiva-charan-git <shivacharan021@gmail.com>
2023-05-28 01:34:39 +0530

    M

--- shipping.sh
+++ shipping.sh
@@ -2,10 +2,11 @@ source common.sh
 
 mysql_root_password=$1
 
-if [ "${mysql_root_password}" == "mysql" ]; then
+if [ -z "${mysql_root_password}" ]; then
    echo -e "\e[31mMissing MySql Root passowrd argument\e[om"
   exit 1
 fi
+
 
 component=shipping
 scheme_type="mysql"
