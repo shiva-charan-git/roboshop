1716da363b5bf5b72f934c7bf47a77f244224977
shiva-charan-git <shivacharan021@gmail.com>
2023-05-27 23:52:29 +0530

    M

--- catalogue.sh
+++ catalogue.sh
@@ -9,7 +9,7 @@ yum install nodejs -y  &>>${log_file}
 status_check $?
 
 print_head "create user roboshop"
-id roboshopb  &>>${log_file}
+id roboshop  &>>${log_file}
 if [ $? -ne 0 ]; then
   useradd roboshop  &>>${log_file}
 fi
