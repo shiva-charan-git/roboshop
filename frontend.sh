92f435bf46cc8cd46d601ba2a7e2d3a23395cb74
shiva-charan-git <shivacharan021@gmail.com>
2023-05-27 16:37:52 +0530

    M

---
+++ frontend.sh
@@ -0,0 +1,13 @@
+yum install nginx -y 
+
+rm -rf /usr/share/nginx/html/* b
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zipb
+
+cp config/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+
+systemctl enable nginx
+systemctl restart nginx 
